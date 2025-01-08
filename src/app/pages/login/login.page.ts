1b630312727300855c0f4927cd04123e4a36ef75
Luis-Troncoso <luis.troncoso.marin@gmail.com>
2025-01-08 19:41:08 -0300

    cambios 2
    se agrego el login y register

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+ingresar() {
+throw new Error('Method not implemented.');
+}
+formularioLogin: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/register/register.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: false,
+})
+export class RegisterPage implements OnInit {
+registrarse() {
+throw new Error('Method not implemented.');
+}
+formularioRegistro: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
